729db1956a8169693cb28c93b50f049a06e86a48
= <=>
2023-11-28 20:38:19 +0530

    Second Commit

--- README.md
+++ README.md
@@ -1,51 +1,66 @@
-Restaurant Recommendation System
+# Restaurant Recommendation System
 Overview
-This program is a simple restaurant recommendation system with a graphical user interface (GUI) built using Tkinter. It allows users to input a cuisine type, and the system recommends restaurants based on the entered cuisine. The recommendations are sorted by restaurant rating and proximity.
+This program is a simple restaurant recommendation system with a **graphical user interface (GUI)** built using Tkinter. It allows users to input a cuisine type, and the system recommends restaurants based on the entered cuisine. The recommendations are sorted by restaurant rating and proximity.
 
-Instructions
-Dependencies:
+# Instructions
+# Dependencies:
 
 Ensure you have Python installed on your system.
 The program uses the pandas library for data handling and tkinter for GUI. You can install them using:
-bash
-Copy code
-pip install pandas
+
+# pip install pandas
 Run the Program:
 
 Execute the main program file restaurant_recommendation_system.py to launch the GUI.
-bash
-Copy code
-python restaurant_recommendation_system.py
-GUI Usage:
 
-Enter the desired cuisine type in the provided entry field.
-Click the "Submit" button to get restaurant recommendations.
-The recommendations will be displayed below the entry field.
+**python restaurant_recommendation_system.py**
+# GUI Usage:
+
+**1.Enter the desired cuisine type in the provided entry field.**
+**2.Click the "Submit" button to get restaurant recommendations.**
+**3.The recommendations will be displayed below the entry field.**
 Unit Tests:
 
 The program includes unit tests to ensure the functionality of the recommendation algorithm.
 Run the unit tests using the following command:
-bash
-Copy code
-python test_recommendations.py
-Additional Information
-Data Structure:
+
+**python test_recommendations.py**
+
+# Additional Information
+## Data Structure:
 
 Restaurant information is stored in a dictionary with the key 'restaurants'.
 Each restaurant is represented by a dictionary with keys 'name', 'cuisine', 'rating', and 'proximity'.
 Recommendation Algorithm:
 
 The program filters restaurants based on user-input cuisine, sorts them by rating and proximity, and displays the recommendations.
-Unit Tests:
+# Unit Tests:
 
 The test_recommendations.py file contains unit tests for the recommendation function.
 The setUp and tearDown methods handle GUI initialization and cleanup for testing.
-Screenshots:
+
+# Screenshots:
 
 Screenshots of the GUI are available in the Git repository for reference.
-Git Repository
-The Git repository for this project includes:
-restaurant_recommendation_system.py: Main program file.
-test_recommendations.py: Unit tests.
-Screenshots folder: GUI screenshots.
-README.md: This documentation file.
+
+
+## Here is Different Cases of Restaurant Recommendation System : 
+**1.GUI SCREEN**: 
+![alt text](https://raw.githubusercontent.com/faizaladhitama/Hierarchical-Multi-Label-Classification-API/master/Capture.JPG "Example")
+
+**2.CASE OF WITHOUT ANY INPUT :**
+
+
+**3.CASE OF SHOWING OUTPUT :**
+
+
+**4.CASE OF NOT VALID INPUT:**
+
+
+## Here is Different Cases of Test Cases File: 
+
+**1.TEST CASES FILE INPUT :**
+
+
+**2.TEST CASES EXCUETION SUMMARY :**
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,63 +1,187 @@
-import unittest
-from unittest.mock import patch
-from tkinter import StringVar, Tk
-from Restaurant_Recommendation_system import recommend_restaurants
-
-class TestRecommendations(unittest.TestCase):
-    restaurant_data = {
-        'restaurants': [
-            {
-                'name': 'Pizza Place',
-                'cuisine': 'Italian',
-                'rating': 4.5,
-                'proximity': 0.5
-            },
-            # ... (other restaurant entries)
-        ]
+import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+# Define the data structure to store restaurant information
+restaurant_data = {
+    'restaurants': [
+        {
+            'name': 'Pizza Place',
+            'cuisine': 'Italian',
+            'rating': 4.5,
+            'proximity': 0.5
+        },
+        {
+            'name': 'Sushi House',
+            'cuisine': 'Japanese',
+            'rating': 4.2,
+            'proximity': 1.2
+        },
+        {
+            'name': 'Taco Bell',
+            'cuisine': 'Mexican',
+            'rating': 3.8,
+            'proximity': 0.8
+        },
+        {
+            'name': 'Curry Palace',
+            'cuisine': 'Indian',
+            'rating': 4.7,
+            'proximity': 1.5
+        },
+        {
+            'name': 'Burger Joint',
+            'cuisine': 'American',
+            'rating': 4.1,
+            'proximity': 0.7
+        },
+        {
+        'name': 'Ramen Spot',
+        'cuisine': 'Japanese',
+        'rating': 4.0,
+        'proximity': 1.0
+    },
+    {
+        'name': 'Pasta House',
+        'cuisine': 'Italian',
+        'rating': 4.3,
+        'proximity': 0.9
+    },
+    {
+        'name': 'Taco Express',
+        'cuisine': 'Mexican',
+        'rating': 4.1,
+        'proximity': 0.6
+    },
+    {
+        'name': 'Spice Haven',
+        'cuisine': 'Indian',
+        'rating': 4.5,
+        'proximity': 1.3
+    },
+    {
+        'name': 'BBQ Grill',
+        'cuisine': 'American',
+        'rating': 4.2,
+        'proximity': 0.5
+    },
+    {
+        'name': 'French Delight',
+        'cuisine': 'French',
+        'rating': 4.4,
+        'proximity': 1.1
+    },
+    {
+        'name': 'Thai Flavor',
+        'cuisine': 'Thai',
+        'rating': 4.6,
+        'proximity': 0.8
+    },
+    {
+        'name': 'Spanish Tapas',
+        'cuisine': 'Spanish',
+        'rating': 4.2,
+        'proximity': 1.4
+    },
+    {
+        'name': 'Chinese Wok',
+        'cuisine': 'Chinese',
+        'rating': 4.3,
+        'proximity': 0.7
+    },
+    {
+        'name': 'Mediterranean Oasis',
+        'cuisine': 'Mediterranean',
+        'rating': 4.5,
+        'proximity': 1.0
     }
+    ]
+}
 
-    def setUp(self):
-        self.root = Tk()
-        self.root.geometry("500x300")  # Set the initial size of the window
+# Function to recommend restaurants based on user input
+# Function to recommend restaurants based on user input
+def recommend_restaurants(cuisine):
+    # Convert user input to lowercase to match data
+    cuisine = cuisine.lower()
 
-    def tearDown(self):
-        self.root.destroy()
+    # Filter restaurants based on cuisine
+    filtered_restaurants = []
+    for restaurant in restaurant_data['restaurants']:
+        if restaurant['cuisine'].lower() == cuisine:
+            filtered_restaurants.append(restaurant)
 
-    def test_recommend_restaurants_valid_cuisine(self):
-        with patch('tkinter.simpledialog.askstring', return_value='italian'):
-            result_text = StringVar()
+    # Sort restaurants by rating and proximity
+    filtered_restaurants.sort(key=lambda restaurant: (restaurant['rating'], restaurant['proximity']), reverse=True)
 
-            with patch('Restaurant_Recommendation_system.result_text', result_text):
-                recommend_restaurants("italian")
+    # Display recommended restaurants with additional details
+    if filtered_restaurants:
+        result_text.set(f"Recommended restaurants for {cuisine}:\n")
+        for restaurant in filtered_restaurants:
+            stars_str = stars(restaurant['rating'])
+            result_text.set(result_text.get() + f"{restaurant['name']} - Rating: {restaurant['rating']}, Stars :  {stars_str}, Distance: {restaurant['proximity']} km\n")
+    else:
+        result_text.set(f"No restaurants found for {cuisine}")
 
-            output = result_text.get().strip()
+# Rest of the code remains the same
 
-            self.assertIn("Recommended restaurants for italian:", output)
-            self.assertIn("Pizza Place", output)
-            self.assertIn("4.5", output)
-            self.assertIn("0.5 km", output)
 
-    def test_recommend_restaurants_invalid_cuisine(self):
-        with patch('tkinter.simpledialog.askstring', return_value='vegetarian'):
-            result_text = StringVar()
-            
-            with patch('Restaurant_Recommendation_system.result_text', result_text):
-                recommend_restaurants("vegetarian")
+# Function to generate stars based on rating
+def stars(rating):
+    stars = ""
+    for i in range(int(rating)):
+        stars += "*"
+    return stars
 
-            output = result_text.get().strip()
+# Modify the on_submit function to take user input and display the result
+def on_submit():
+    cuisine_input = cuisine_var.get().strip()  # Get user input and remove leading/trailing spaces
+    if cuisine_input:
+        recommend_restaurants(cuisine_input)
+    else:
+        result_text.set("Please enter a cuisine type.")
 
-            self.assertIn("No restaurants found for vegetarian", output)
+# Create GUI
+def on_submit():
+    cuisine_input = cuisine_var.get().strip()  # Get user input and remove leading/trailing spaces
+    if cuisine_input:
+        recommend_restaurants(cuisine_input)
+    else:
+        result_text.set("Please enter a cuisine type.")
 
-    def test_recommend_restaurants_empty_input(self):
-        with patch('tkinter.simpledialog.askstring', return_value=''):
-            result_text = StringVar()
+# Create GUI
+def on_submit():
+    cuisine_input = cuisine_var.get().strip()  # Get user input and remove leading/trailing spaces
+    if cuisine_input:
+        recommend_restaurants(cuisine_input)
+    else:
+        result_text.set("Please enter a cuisine type.")
 
-            with patch('Restaurant_Recommendation_system.result_text', result_text):
-                recommend_restaurants("")
+# Create GUI
+def on_submit():
+    cuisine_input = cuisine_var.get().strip()  # Get user input and remove leading/trailing spaces
+    if cuisine_input:
+        recommend_restaurants(cuisine_input)
+    else:
+        result_text.set("Please enter a cuisine type.")
 
-            output = result_text.get().strip()
+root = tk.Tk()
+root.title("Restaurant Recommendation System")
 
-            self.assertIn("No restaurants found for", output)
+# Set the initial size of the window
+root.geometry("500x300")
 
-if __name__ == '__main__':
-    unittest.main()
+# GUI components
+label = ttk.Label(root, text="Enter the type of cuisine you desire:")
+label.pack(pady=10)
+
+cuisine_var = tk.StringVar()
+cuisine_entry = ttk.Entry(root, textvariable=cuisine_var)
+cuisine_entry.pack(pady=10)
+
+submit_button = ttk.Button(root, text="Submit", command=on_submit)
+submit_button.pack(pady=10)
+
+result_text = tk.StringVar()
+result_label = ttk.Label(root, textvariable=result_text)
+result_label.pack(pady=10)
+
+root.mainloop()
